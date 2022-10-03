621bfc42f0787b9e52668c2776b4943055e4db1f
MarcoToledo <marcodavidtoledo@gmail.com>
2022-10-03 03:47:28 -0400

    Terminé mi issue, siuuu

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html lang="">
+<html lang="en">
 
 <head>
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://vjs.zencdn.net/5-unsafe/video-js.css">
-
     <!-- boostrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/inicio.css')}}">
     <title>Inicio</title>
 
-    <!-- @vite(['public/css/inicio.css','public/js/inicio.js','resources/sass/_inicio.scss']) -->
 </head>
 
 <body>
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['public/css/inicio.css',
+            input: [
                 'public/js/inicio.js',
-                'resources/sass/_inicio.scss',
             ],
             refresh: true,
         }),
